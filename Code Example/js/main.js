b85b349357d5b9d643d0c4b1fe4770a93516d62a
Ibrahim Abushunnar <ibrahimimad256@gmail.com>
2023-01-23 18:33:37 -0500

    CSS & JS

---
+++ Code Example/js/main.js
@@ -0,0 +1,8 @@
+console.log("Script loaded")
+console.log("test");;
+
+button = document.querySelector("button");
+button.onclick = () => {
+    console.log("YOU CLICKED ME");
+    window.location.href = "https://youtu.be/71Gt46aX9Z4";
+};
